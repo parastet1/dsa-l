0bae463a179af43342cc4806af25ef473ebb79e0
Tanuj Paraste <tanuj.paraste1@gmail.com>
2023-12-04 22:57:48 +0530

    added files: O(logn)

---
+++ 101-big_o/src/live/tanujdevops/Main6.java
@@ -0,0 +1,28 @@
+package live.tanujdevops;
+
+public class Main6 {
+	public static void main(String[] args) {
+		int[] collection = { 1, 2, 3, 4, 5, 6 };
+		int location = findLocation(6, collection);
+		System.out.println(location);
+	}
+
+//	time complexity : O(logn)
+	public static int findLocation(int n, int[] collection) {
+		int left = 0;
+		int right = collection.length - 1;
+		int mid;
+		while (left <= right) {
+			mid = left + (right - left) / 2;
+			if (n == collection[mid]) {
+				return mid;
+			} else if (n > collection[mid]) {
+				left = mid + 1;
+			} else {
+				right = mid - 1;
+			}
+		}
+
+		return -1;
+	}
+}
