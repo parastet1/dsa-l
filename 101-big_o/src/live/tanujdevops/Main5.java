ff50b93dc9f371148571dc1352db04dd33945950
Tanuj Paraste <tanuj.paraste1@gmail.com>
2023-12-04 22:14:32 +0530

    updated files: O(1)

--- 101-big_o/src/live/tanujdevops/Main5.java
+++ 101-big_o/src/live/tanujdevops/Main5.java
@@ -2,13 +2,13 @@ package live.tanujdevops;
 
 public class Main5 {
 	public static void main(String[] args) {
-		addItems(10);
+		addItems(100000);
 	}
 
-//	time complexity : O(1)
+//	time complexity : O(2) == O(1)
 //	no matter how big the value of n is, the number of operation remains constant
-//	there is only one addition operation
+//	there is only 2 addition operation
 	public static int addItems(int n) {
-		return n + n;
+		return n + n + n;
 	}
 }
