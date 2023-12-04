ae5a424ee2aad62bf65754b34339442013cca399
Tanuj Paraste <tanuj.paraste1@gmail.com>
2023-12-04 21:00:58 +0530

    added files: O(n)

---
+++ 101-big_o/src/live/tanujdevops/Main.java
@@ -0,0 +1,13 @@
+package live.tanujdevops;
+
+public class Main {
+	public static void main(String[] args) {
+		printItems(10);
+	}
+
+	public static void printItems(int n) {
+		for (int i = 0; i < n; i++) {
+			System.out.println(i);
+		}
+	}
+}
