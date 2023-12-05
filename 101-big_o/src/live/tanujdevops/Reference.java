160da9182b68e971c2164fda0812edb8370b0d1e
Tanuj Paraste <tanuj.paraste1@gmail.com>
2023-12-05 11:03:40 +0530

    added files: Big O reference time complexity

---
+++ 101-big_o/src/live/tanujdevops/Reference.java
@@ -0,0 +1,14 @@
+package live.tanujdevops;
+
+public class Reference {
+	/*
+	 * O(n * n) : loop within a loop
+	 * 
+	 * O(n) : linear
+	 * 
+	 * O(logn) : divide and conquer
+	 * 
+	 * O(1) : constant
+	 * 
+	 */
+}
