d9ab0db6711e38c681e8f682203121fa194e90cd
Tanuj Paraste <tanuj.paraste1@gmail.com>
2023-12-05 10:59:03 +0530

    added files: time complexity array operations, dynamic arrays

---
+++ 101-big_o/src/live/tanujdevops/MyArrayList.java
@@ -0,0 +1,40 @@
+package live.tanujdevops;
+
+public class MyArrayList {
+//	add an element at the end of the array
+//	O(1)
+	public int add(int e) {
+		return -1;
+	}
+
+//	add at a particular index
+//	O(n)
+	public int insert(int idx, int e) {
+		return -1;
+	}
+
+//	remove an element from the end of the array
+//	O(1)
+	public int remove() {
+		return -1;
+	}
+
+//	remove at a particular index
+//	O(n)
+	public int remove(int idx) {
+		return -1;
+	}
+
+//	find by linear search
+//	O(n)
+	public boolean includes(int e) {
+//		iterate the array here
+		return false;
+	}
+
+//	find element at a particular index
+//	O(1)
+	public int get(int idx) {
+		return -1;
+	}
+}
