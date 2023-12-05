50c621abb690bbe15f9f985b1461cf3acd91ef02
Tanuj Paraste <tanuj.paraste1@gmail.com>
2023-12-05 11:05:04 +0530

    updated files: Big O reference time complexity

--- 101-big_o/src/live/tanujdevops/Reference.java
+++ 101-big_o/src/live/tanujdevops/Reference.java
@@ -4,11 +4,13 @@ public class Reference {
 	/*
 	 * O(n * n) : loop within a loop
 	 * 
-	 * O(n) : linear
+	 * O(n) : linear or proportional
 	 * 
 	 * O(logn) : divide and conquer
 	 * 
 	 * O(1) : constant
 	 * 
+	 * 
+	 * https://www.bigocheatsheet.com/
 	 */
 }
