d7fed9a68a2686396aacd507c130c7af25c20471
Tanuj Paraste <tanuj.paraste1@gmail.com>
2023-12-06 16:19:44 +0530

    added files: linked list constructor

---
+++ 201-LinkedList/src/live/tanujdevops/LinkedList.java
@@ -0,0 +1,67 @@
+package live.tanujdevops;
+
+/*
+ * 
+ * singly linked list
+ * 
+ * */
+
+public class LinkedList {
+	private Node head;
+	private Node tail;
+	private int length;
+
+	class Node {
+		int value;
+		Node next;
+
+		Node(int value) {
+			this.value = value;
+		}
+	}
+
+	public LinkedList(int value) {
+		var newNode = new Node(value);
+		head = newNode;
+		tail = newNode;
+		length = 1;
+	}
+
+	public void getHead() {
+		System.out.println("head: " + head.value);
+	}
+
+	public void getTail() {
+		System.out.println("tail: " + tail.value);
+	}
+
+	public void getLength() {
+		System.out.println("length: " + length);
+	}
+
+	public void printList() {
+		var temp = head;
+		while (temp != null) {
+			System.out.print(temp.value + " --> ");
+			temp = temp.next;
+		}
+	}
+
+	public void printAll() {
+		if (length == 0) {
+			System.out.println("head: null");
+			System.out.println("tail: null");
+		} else {
+			System.out.println("head: " + head.value);
+			System.out.println("tail: " + tail.value);
+		}
+
+		System.out.println("Length: " + length);
+		System.out.println("Linked List:");
+		if (length == 0) {
+			System.out.println("empty");
+		} else {
+			printList();
+		}
+	}
+}
---
+++ 201-LinkedList/src/live/tanujdevops/Main.java
@@ -0,0 +1,11 @@
+package live.tanujdevops;
+
+public class Main {
+	public static void main(String[] args) {
+		var myLinkedList = new LinkedList(4);
+		myLinkedList.getHead();
+		myLinkedList.getTail();
+		myLinkedList.getLength();
+		myLinkedList.printList();
+	}
+}
