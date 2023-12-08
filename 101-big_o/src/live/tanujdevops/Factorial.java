bee9bb84202fd79aa30825edd2204c476ab129bf
Tanuj Paraste <tanuj.paraste1@gmail.com>
2023-12-08 17:18:11 +0530

    updated file: recursion factorial O(n)

--- 101-big_o/src/live/tanujdevops/Factorial.java
+++ 101-big_o/src/live/tanujdevops/Factorial.java
@@ -7,6 +7,11 @@ public class Factorial {
 		System.out.printf("%d! = %d", n, factorial(n));
 	}
 
+//	Recursive relation:
+//	T(n) = a, n <= 1
+//	     = b + T(n-1), n > 1
+//	
+//	     = O(n)
 	private static int factorial(int n) {
 		if (n <= 1) {
 			return 1;
