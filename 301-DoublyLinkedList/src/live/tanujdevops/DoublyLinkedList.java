69d488d4f0925eeaafcab2d9cd19b868289b86fa
Tanuj Paraste <tanuj.paraste1@gmail.com>
2023-12-12 15:17:36 +0530

    added files: linked list doubly linked list

---
+++ 301-DoublyLinkedList/src/live/tanujdevops/DoublyLinkedList.java
@@ -0,0 +1,45 @@
+package live.tanujdevops;
+
+public class DoublyLinkedList {
+	Node head;
+	Node tail;
+	int length;
+
+	public DoublyLinkedList(int value) {
+		Node newNode = new Node(value);
+		head = newNode;
+		tail = newNode;
+		length = 1;
+	}
+
+	class Node {
+		int value;
+		Node next;
+		Node prev;
+
+		Node(int value) {
+			this.value = value;
+		}
+	}
+
+	public void getHead() {
+		System.out.println("head: " + head.value);
+	}
+
+	public void getTail() {
+		System.out.println("tail: " + tail.value);
+	}
+
+	public void getLength() {
+		System.out.println("length: " + length);
+	}
+
+	public void printList() {
+		Node temp = head;
+		while (temp != null) {
+			System.out.println(temp.value);
+			temp = temp.next;
+		}
+	}
+
+}
---
+++ 301-DoublyLinkedList/src/live/tanujdevops/Main.java
@@ -0,0 +1,12 @@
+package live.tanujdevops;
+
+public class Main {
+	public static void main(String[] args) {
+		DoublyLinkedList myDLL = new DoublyLinkedList(1);
+		myDLL.getHead();
+		myDLL.getTail();
+		myDLL.getLength();
+
+		myDLL.printList();
+	}
+}
